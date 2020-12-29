04acbfe0c8249e39417b537776904e37a3e65063
Vladislav Startsev <vladiska777@gmail.com>
2020-12-29 15:28:43 +0400

    * Programm can now read paragraph run by run.   and return exact font.name, font.size

--- ed2.py
+++ ed2.py
@@ -2,12 +2,26 @@
 import docx 
 from docx.shared import Pt
 
-doc = docx.Document('f.docx') 
+doc = docx.Document('hand.docx') 
 print(len(doc.paragraphs))
-c = doc.paragraphs[0].style.font.color.rgb
+par1 = doc.paragraphs[0]
+t = []
+sizes = []
+names = []
+for run in par1.runs:
+  t.append(run.text)
 
-
-print(c)
+  if run.font.size is not None: 
+   sizes.append(str(run.font.size)) 
+    
+  if run.font.name is not None: 
+   names.append(str(run.font.name)) 
+   
+#for i in range(len(t)): 
+# print(t[i])
+print(' '.join(t)) 
+print(' '.join(sizes)) 
+print(' '.join(names)) 
 
 
 
